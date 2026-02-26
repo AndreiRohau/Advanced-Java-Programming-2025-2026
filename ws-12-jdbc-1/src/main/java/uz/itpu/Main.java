f7f926726aba554d5d88f9aa850e968a03ce2041
arohau <arohau@edmunds.com>
2026-02-26 15:00:28 +0300

    ws-12

---
+++ ws-12-jdbc-1/src/main/java/uz/itpu/Main.java
@@ -0,0 +1,8 @@
+package uz.itpu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Introduction to JDBC");
+        System.out.println("Creating Database Queries Using Statement Objects");
+    }
+}
---
+++ ws-14-jdbc-2/src/main/java/uz/itpu/Main.java
@@ -0,0 +1,9 @@
+package uz.itpu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Working With Transactions Using JDBC");
+        System.out.println("Connection Pool");
+        System.out.println("Metadata");
+    }
+}
