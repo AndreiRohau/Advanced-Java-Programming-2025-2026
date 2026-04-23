3f36eb3ae4026a667c1eeeadce268e2fa2dacfe7
arohau <arohau@edmunds.com>
2026-04-23 13:48:41 +0300

    ws-22-5

--- ws-22-threads-and-synchronization-4/src/main/java/uz/itpu/pt1/ex1/MyThread.java
+++ ws-22-threads-and-synchronization-4/src/main/java/uz/itpu/pt1/ex1/MyThread.java
@@ -2,7 +2,7 @@ package uz.itpu.pt1.ex1;
 
 class MyThread extends Thread {
     public void run() {
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println("Thread: " + getName() + " i=" + i);
             try {
                 Thread.sleep(1000);
